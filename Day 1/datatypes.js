791bdf7f48bb776c9362e9161d1774dee9869b5d
sharonlhs <121072143+sharonlhs@users.noreply.github.com>
2023-04-22 21:29:49 +0800

    Day 1 Done

---
+++ Day 1/datatypes.js
@@ -0,0 +1,39 @@
+// let firstName = "Sharon";
+// let hasGraduated = true;
+// let country
+// let city = null;
+
+// 5. typeof operator to check different data types. 
+// console.log(typeof firstName, typeof hasGraduated, 
+//     typeof country, typeof city);
+
+//6. Declare four variables without assigning values
+//let  firstName, lastName, isMarried, country, age;
+
+//7&8. Declare four variables with assigned values
+/* 
+let firstName = "Sharon";
+let lastName = "lhs";
+let isMarried = false;
+let country = "Malaysia";
+let age = 23; 
+*/
+
+/*9. Declare variables to store your first name, 
+last name, marital status, country and age in multiple 
+lines */
+
+let firstName = "Sharon",
+    lastName = "lhs",
+    isMarried = false,
+    country = "Malaysia",
+    age = 23;
+
+/*10. Declare two variables myAge and yourAge and assign them 
+initial values and log to the browser console. */
+
+let myAge = age;
+let yourAge = 30;
+
+console.log(`I am ${myAge} years old.`);
+console.log(`You are ${yourAge} years old.`);
---
+++ Day 1/variables.js
@@ -0,0 +1,20 @@
+//1. comments can make code readable
+
+//2. Welcome to 30DaysOfJavaScript
+
+/* 3. comments can make code readable, 
+    easy to reuse and informative
+*/
+
+/* 4. Create a variable.js file and declare variables 
+and assign string, boolean, undefined and null data types
+*/
+
+let firstName = "Sharon";
+let hasGraduated = true;
+let country
+let city = null;
+
+//typeof to check data types
+console.log(typeof firstName, typeof hasGraduated, 
+    typeof country, typeof city);
